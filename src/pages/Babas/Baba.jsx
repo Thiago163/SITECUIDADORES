b9bdd423f06539b4eac4ca3b1eef811b9d29defa
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-04-27 22:27:42 -0300

    novo fetch

--- src/pages/Babas/Baba.jsx
+++ src/pages/Babas/Baba.jsx
@@ -7,7 +7,7 @@ function Jobs() {
   const [vagas, setVagas] = useState([]);
 
   useEffect(() => {
-    fetch("https://localhost:44396/api/cuidadores", {
+    fetch("https://appcuidadores.azurewebsites.net/api/cuidadores", {
       method: "GET",
     })
       .then((response) => response.json())
