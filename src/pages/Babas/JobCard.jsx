8adffc86fac1b96ae01a2527dd06033b10e5065d
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-03 18:51:46 -0300

    Update JobCard.jsx

--- src/pages/Babas/JobCard.jsx
+++ src/pages/Babas/JobCard.jsx
@@ -1,39 +1,37 @@
-import React from "react";
 import "./JobCard.css";
 
 function JobCard({ vaga }) {
-  const handlePortfolioClick = () => {
-    window.location.href = vaga.link;
-  };
+    const handleClick = () => {
+        window.location.href = vaga.link;
+    };
 
-  return (
-    <div className="card">
-      <div className="company-logo">
-        <img src={vaga.imagem} alt="Foto não cadastrada" />
-      </div>
-      <div className="text-area">
-        <div className="title-area">
-          <h1>Nome: {vaga.nome}</h1>
-          <h2>Preço cobrado: R${vaga.preco} por dia</h2>
+    return (
+        <div className="card">
+            <div className="company-logo">
+                <img src={vaga.imagem} alt="Foto não cadastrada"></img>
+            </div>
+            <div className="text-area">
+                <div className="title-area">
+                    <h1>Nome: {vaga.nome}</h1>
+                    <h2>Preço cobrado: R${vaga.preco} por dia</h2>
+                </div>
+                <div className="content-area">
+                    <h2>Descrição do cuidador:</h2>
+                    <p>{vaga.descricao}</p>
+                    <h2>
+                    {vaga.estado}, {vaga.cidade}
+                    </h2>
+                    <button className="portiButton" onClick={handleClick}>
+                        Portfólio
+                    </button>
+
+                </div>
+                <div className="footer">
+                    <p>Celular: {vaga.celular} Email: {vaga.email}</p>
+                </div>
+            </div>
         </div>
-        <div className="content-area">
-          <h2>Descrição do cuidador:</h2>
-          <p>{vaga.descricao}</p>
-          <p>
-            {vaga.estado}, {vaga.cidade}
-          </p>
-          <button className="portfolioButton" onClick={handlePortfolioClick}>
-            Portfólio
-          </button>
-        </div>
-        <div className="footer">
-          <p>
-            Celular: {vaga.celular} | Email: {vaga.email}
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default JobCard;
