f5fe92ffa24dd1cb0e87ef7947c468455e4ab8de
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-05 20:21:58 -0300

    Update JobCard.jsx

--- src/pages/Babas/JobCard.jsx
+++ src/pages/Babas/JobCard.jsx
@@ -8,7 +8,7 @@ function JobCard({ vaga }) {
     return (
         <div className="card">
             <div className="company-logo">
-                <img src={vaga.imagem}></img>
+                <img src={vaga.imagem}> alt="Foto não cadastrada"</img>
             </div>
             <div className="text-area">
                 <div className="title-area">
