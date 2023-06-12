4ef2ec2e1f50fcac1422a28e06efceda4747aa77
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-11 21:55:54 -0300

    Update JobCard.jsx

--- src/pages/Babas/JobCard.jsx
+++ src/pages/Babas/JobCard.jsx
@@ -1,37 +1,46 @@
-import "./JobCard.css";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './JobCard.css';
 
 function JobCard({ vaga }) {
-    const handleClick = () => {
-        window.location.href = vaga.link;
-    };
+  const navigate = useNavigate();
 
-    return (
-        <div className="card">
-            <div className="company-logo">
-                <img src={vaga.imagem} alt="Imagem não cadastrada"/>
-            </div>
-            <div className="text-area">
-                <div className="title-area">
-                    <h1>Nome: {vaga.nome}</h1>
-                    <h2>Preço cobrado: R${vaga.preco} por dia</h2>
-                </div>
-                <div className="content-area">
-                    <h2>Descrição do cuidador:</h2>
-                    <p>{vaga.descricao}</p>
-                    <h2>
-                    {vaga.cidade} - {vaga.estado}
-                    </h2>
-                    <button className="portiButton" onClick={handleClick}>
-                        Portfólio
-                    </button>
+  const handleClick = () => {
+    navigate('/aplicativo');
+  };
 
-                </div>
-                <div className="footer">
-                    <p>Celular: {vaga.celular} | Email: {vaga.email}</p>
-                </div>
-            </div>
+  const handlePortfolioClick = (event) => {
+    event.stopPropagation(); // Impede que o evento de clique no card seja acionado
+
+    window.location.href = vaga.link;
+  };
+
+  return (
+    <div className="card" onClick={handleClick}>
+      <div className="company-logo">
+        <img src={vaga.imagem} alt="Imagem não cadastrada" />
+      </div>
+      <div className="text-area">
+        <div className="title-area">
+          <h1>Nome: {vaga.nome}</h1>
+          <h2>Preço cobrado: R${vaga.preco} por dia</h2>
         </div>
-    );
+        <div className="content-area">
+          <h2>Descrição do cuidador:</h2>
+          <p>{vaga.descricao}</p>
+          <h2>
+            {vaga.cidade} - {vaga.estado}
+          </h2>
+          <button className="portiButton" onClick={handlePortfolioClick}>
+            Portfólio
+          </button>
+        </div>
+        <div className="footer">
+          <p>Celular: {vaga.celular} | Email: {vaga.email}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default JobCard;
